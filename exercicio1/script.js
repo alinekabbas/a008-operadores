964be81fc704eb0f6bdbd3cfe5b60e1d7f2bf606
alinekabbas <alinekabbas@gmail.com>
2022-08-30 13:18:42 -0300

    resolução exercicio 1

---
+++ exercicio1/script.js
@@ -0,0 +1,6 @@
+let numUsuario = Number (prompt("Insira um número par"))
+let numDivisor = 2
+console.log(numUsuario % numDivisor)
+
+//3- O resultado sempre será 0, pois todo numero par dividido por 2 não terá resto.
+//4- Aparece o resto da divisão.
